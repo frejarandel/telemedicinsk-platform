997b060b0801600ebc81296d5f1e0614ca372f57
frejarandel <freja.randel@gmail.com>
2026-04-23 12:52:00 +0200

    Update TURN server URL in scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -32,7 +32,7 @@ let peerConfiguration = {
             urls: "stun:stun.l.google.com:19302"
         },
         {
-            urls: "turn:global.relay.metered.ca:80",
+            urls: 'turn:openrelay.metered.ca:80',
             username: "openrelayproject",
             credential: "openrelayproject"
         }
